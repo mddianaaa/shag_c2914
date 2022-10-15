3decbf2528ff024897268617c3b92aecd854d438
mddianaaa <diana.makater@gmail.com>
2022-10-15 18:00:44 +0300

    dz2

---
+++ para2_1.py
@@ -0,0 +1,12 @@
+class Student:
+    print('Hi!')
+    def __init__(self, height=160):
+        self.height = height
+
+
+first_ek = Student()
+print(first_ek.height)
+
+dima = Student(height=186)
+print(dima.height)
+
