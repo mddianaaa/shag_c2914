8c5fc1492cdc3d08e931f1eff905c79b2c8f680c
mddianaaa <diana.makater@gmail.com>
2022-10-31 17:16:30 +0200

    dz6

---
+++ dz6.py
@@ -0,0 +1,7 @@
+import colorama
+
+for a in dir(colorama):
+    print(a)
+
+
+print("Я вважаю, що найважливішими атрибутами є:Back, Fore та Style. За їх допомогою можна змінювати колір, стиль та фон тексту.")
